d0ee30452a2484d74c6b022aff33330d7f17428b
Diego González López <diego.gonzalez@xeridia.com>
2022-08-04 10:52:57 +0200

    FARQXER-313 Basic SpringBoot generated structure

---
+++ main/java/com/xeridia/xercoffeeshop/XerCoffeShopApplication.java
@@ -0,0 +1,13 @@
+package com.xeridia.xercoffeeshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XerCoffeShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XerCoffeShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/xeridia/xercoffeeshop/XerCoffeShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xeridia.xercoffeeshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XerCoffeShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
